e31c2258423cbf35739104a4b546ee46a4f78176
Patrizio <patrizio.pezzilli@nexi.it>
2020-01-21 14:40:12 +0100

    fix capitalized

--- src/main/java/com/ristorantemonopoli/backend/service/impl/MenuDelGiornoServiceImpl.java
+++ src/main/java/com/ristorantemonopoli/backend/service/impl/MenuDelGiornoServiceImpl.java
@@ -5,6 +5,7 @@ import com.ristorantemonopoli.backend.database.repository.MenuDelGiornoRepositor
 import com.ristorantemonopoli.backend.dto.MenuDelGiornoSaveRequest;
 import com.ristorantemonopoli.backend.dto.PastoDTO;
 import com.ristorantemonopoli.backend.service.MenuDelGiornoService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -40,15 +41,27 @@ public class MenuDelGiornoServiceImpl implements MenuDelGiornoService {
             repository.deleteAll();
 
             if (null != request.getPrimi()) {
-                request.getPrimi().forEach(p -> repository.save(new MenuDelGiorno(p.getNome(), p.getPrezzo(), p.getCategoria())));
+                request.getPrimi().forEach(p -> {
+                    String capitalizedName = (StringUtils.capitalize(p.getNome()));
+                    p.setNome(capitalizedName);
+                    repository.save(new MenuDelGiorno(p.getNome(), p.getPrezzo(), p.getCategoria()));
+                });
             }
 
             if (null != request.getSecondi()) {
-                request.getSecondi().forEach(p -> repository.save(new MenuDelGiorno(p.getNome(), p.getPrezzo(), p.getCategoria())));
+                request.getSecondi().forEach(p -> {
+                    String capitalizedName = (StringUtils.capitalize(p.getNome()));
+                    p.setNome(capitalizedName);
+                    repository.save(new MenuDelGiorno(p.getNome(), p.getPrezzo(), p.getCategoria()));
+                });
             }
 
             if (null != request.getPizze()) {
-                request.getPizze().forEach(p -> repository.save(new MenuDelGiorno(p.getNome(), p.getPrezzo(), p.getCategoria())));
+                request.getPizze().forEach(p -> {
+                    String capitalizedName = (StringUtils.capitalize(p.getNome()));
+                    p.setNome(capitalizedName);
+                    repository.save(new MenuDelGiorno(p.getNome(), p.getPrezzo(), p.getCategoria()));
+                });
             }
         }
     }
