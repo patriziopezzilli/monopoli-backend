95af8a2dff8556985f6d749c7343dbd38e68458d
Patrizio <patrizio.pezzilli@nexi.it>
2020-01-26 19:18:51 +0100

    aggiunta gestione descrizione e recensione

--- src/main/java/com/ristorantemonopoli/backend/controller/UtilityController.java
+++ src/main/java/com/ristorantemonopoli/backend/controller/UtilityController.java
@@ -1,8 +1,8 @@
 package com.ristorantemonopoli.backend.controller;
 
+import com.ristorantemonopoli.backend.dto.ProgressDTO;
 import com.ristorantemonopoli.backend.service.SubscriberService;
 import com.ristorantemonopoli.backend.service.VisitorService;
-import com.ristorantemonopoli.backend.dto.ProgressDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,21 +27,55 @@ public class UtilityController {
         ProgressDTO visitatori = new ProgressDTO();
         visitatori.setTitle("Visitatori del Sito");
         visitatori.setValue(service.countVisitors());
-        visitatori.setActiveProgress(70);
+
+        Integer activeProgress = 0;
+        if (visitatori.getValue() > 0 && visitatori.getValue() <= 100) {
+            activeProgress = ((visitatori.getValue() * 100) / 100);
+        } else if (visitatori.getValue() > 100 && visitatori.getValue() <= 1000) {
+            activeProgress = ((visitatori.getValue() * 100) / 1000);
+        } else if (visitatori.getValue() > 1000 && visitatori.getValue() <= 10000) {
+            activeProgress = ((visitatori.getValue() * 100) / 10000);
+        } else {
+            activeProgress = 100;
+        }
+        visitatori.setActiveProgress(activeProgress);
+
         visitatori.setDescription("Visite effettuate su www.ristorantemonopoli.com");
 
         ProgressDTO menuDelGiornoSubscriber = new ProgressDTO();
         menuDelGiornoSubscriber.setTitle("Registrati al Menù del Giorno");
         menuDelGiornoSubscriber.setValue(subscriberService.countMenuSubscriber());
-        menuDelGiornoSubscriber.setActiveProgress(0);
+
+        Integer activeProgress2 = 0;
+        if (menuDelGiornoSubscriber.getValue() > 0 && menuDelGiornoSubscriber.getValue() <= 100) {
+            activeProgress2 = ((menuDelGiornoSubscriber.getValue() * 100) / 100);
+        } else if (menuDelGiornoSubscriber.getValue() > 100 && menuDelGiornoSubscriber.getValue() <= 1000) {
+            activeProgress2 = ((menuDelGiornoSubscriber.getValue() * 100) / 1000);
+        } else if (menuDelGiornoSubscriber.getValue() > 1000 && menuDelGiornoSubscriber.getValue() <= 10000) {
+            activeProgress2 = ((menuDelGiornoSubscriber.getValue() * 100) / 10000);
+        } else {
+            activeProgress2 = 100;
+        }
+        menuDelGiornoSubscriber.setActiveProgress(activeProgress2);
+
         menuDelGiornoSubscriber.setDescription("Registrati alla newsletter del menù del giorno");
 
         ProgressDTO courtesySubscriber = new ProgressDTO();
         courtesySubscriber.setTitle("Registrati di Cortesia");
         courtesySubscriber.setValue(subscriberService.countCourtesySubscriber());
-        courtesySubscriber.setActiveProgress(10);
-        courtesySubscriber.setDescription("Registrati alla pagina di cortesia su www.ristorantemonopoli.com");
 
+        Integer activeProgress3 = 0;
+        if (courtesySubscriber.getValue() > 0 && courtesySubscriber.getValue() <= 100) {
+            activeProgress3 = ((courtesySubscriber.getValue() * 100) / 100);
+        } else if (courtesySubscriber.getValue() > 100 && courtesySubscriber.getValue() <= 1000) {
+            activeProgress3 = ((courtesySubscriber.getValue() * 100) / 1000);
+        } else if (courtesySubscriber.getValue() > 1000 && courtesySubscriber.getValue() <= 10000) {
+            activeProgress3 = ((courtesySubscriber.getValue() * 100) / 10000);
+        } else {
+            activeProgress3 = 100;
+        }
+        courtesySubscriber.setActiveProgress(activeProgress3);
+        courtesySubscriber.setDescription("Registrati alla pagina di cortesia su www.ristorantemonopoli.com");
         progressDTOS.add(visitatori);
         progressDTOS.add(menuDelGiornoSubscriber);
         progressDTOS.add(courtesySubscriber);
