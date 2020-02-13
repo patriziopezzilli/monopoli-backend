403ad7f832b065660770a51566f564ef918c804c
Patrizio <patrizio.pezzilli@nexi.it>
2020-02-13 22:28:25 +0100

    theme

--- src/main/java/com/ristorantemonopoli/backend/utils/TemplateUtils.java
+++ src/main/java/com/ristorantemonopoli/backend/utils/TemplateUtils.java
@@ -15,12 +15,12 @@ public class TemplateUtils {
             "    <style type=\"text/css\">\n" +
             "    a {text-decoration: none;}\n" +
             "    </style>\n" +
-            "    <![endif]--> \n" +
-            "  <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]--> \n" +
+            "     \n" +
+            "  <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style> \n" +
             "  <!--[if !mso]><!-- --> \n" +
             "  <link href=\"https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i\" rel=\"stylesheet\"> \n" +
             "  <link href=\"https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i\" rel=\"stylesheet\"> \n" +
-            "  <!--<![endif]--> \n" +
+            "  <!-- \n" +
             "  <style type=\"text/css\">\n" +
             "@media only screen and (max-width:600px) {p, ul li, ol li, a { font-size:17px!important; line-height:150%!important } h1 { font-size:30px!important; text-align:center; line-height:120%!important } h2 { font-size:26px!important; text-align:left; line-height:120%!important } h3 { font-size:20px!important; text-align:left; line-height:120%!important } h1 a { font-size:30px!important; text-align:center } h2 a { font-size:20px!important; text-align:left } h3 a { font-size:20px!important; text-align:left } .es-menu td a { font-size:16px!important } .es-header-body p, .es-header-body ul li, .es-header-body ol li, .es-header-body a { font-size:16px!important } .es-footer-body p, .es-footer-body ul li, .es-footer-body ol li, .es-footer-body a { font-size:17px!important } .es-infoblock p, .es-infoblock ul li, .es-infoblock ol li, .es-infoblock a { font-size:12px!important } *[class=\"gmail-fix\"] { display:none!important } .es-m-txt-c, .es-m-txt-c h1, .es-m-txt-c h2, .es-m-txt-c h3 { text-align:center!important } .es-m-txt-r, .es-m-txt-r h1, .es-m-txt-r h2, .es-m-txt-r h3 { text-align:right!important } .es-m-txt-l, .es-m-txt-l h1, .es-m-txt-l h2, .es-m-txt-l h3 { text-align:left!important } .es-m-txt-r img, .es-m-txt-c img, .es-m-txt-l img { display:inline!important } .es-button-border { display:inline-block!important } a.es-button { font-size:14px!important; display:inline-block!important } .es-btn-fw { border-width:10px 0px!important; text-align:center!important } .es-adaptive table, .es-btn-fw, .es-btn-fw-brdr, .es-left, .es-right { width:100%!important } .es-content table, .es-header table, .es-footer table, .es-content, .es-footer, .es-header { width:100%!important; max-width:600px!important } .es-adapt-td { display:block!important; width:100%!important } .adapt-img { width:100%!important; height:auto!important } .es-m-p0 { padding:0px!important } .es-m-p0r { padding-right:0px!important } .es-m-p0l { padding-left:0px!important } .es-m-p0t { padding-top:0px!important } .es-m-p0b { padding-bottom:0!important } .es-m-p20b { padding-bottom:20px!important } .es-mobile-hidden, .es-hidden { display:none!important } .es-desk-hidden { display:table-row!important; width:auto!important; overflow:visible!important; float:none!important; max-height:inherit!important; line-height:inherit!important } .es-desk-menu-hidden { display:table-cell!important } table.es-table-not-adapt, .esd-block-html table { width:auto!important } table.es-social { display:inline-block!important } table.es-social td { display:inline-block!important } }\n" +
             "#outlook a {\n" +
@@ -71,7 +71,7 @@ public class TemplateUtils {
             "\t\t\t<v:background xmlns:v=\"urn:schemas-microsoft-com:vml\" fill=\"t\">\n" +
             "\t\t\t\t<v:fill type=\"tile\" color=\"#fefdfd\"></v:fill>\n" +
             "\t\t\t</v:background>\n" +
-            "\t\t<![endif]--> \n" +
+            "\t\t \n" +
             "   <table class=\"es-wrapper\" width=\"100%\" cellspacing=\"0\" cellpadding=\"0\" style=\"mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;padding:0;Margin:0;width:100%;height:100%;background-repeat:repeat;background-position:center top;\"> \n" +
             "     <tr style=\"border-collapse:collapse;\"> \n" +
             "      <td valign=\"top\" style=\"padding:0;Margin:0;\"> \n" +
@@ -271,7 +271,7 @@ public class TemplateUtils {
             "           <table bgcolor=\"#ffffff\" class=\"es-content-body\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" width=\"600\" style=\"mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:#FFFFFF;\"> \n" +
             "             <tr style=\"border-collapse:collapse;\"> \n" +
             "              <td align=\"left\" style=\"padding:0;Margin:0;padding-top:20px;padding-left:40px;padding-right:40px;\"> \n" +
-            "               <!--[if mso]><table width=\"520\" cellpadding=\"0\" cellspacing=\"0\"><tr><td width=\"250\" valign=\"top\"><![endif]--> \n" +
+            "               <!--[if mso]><table width=\"520\" cellpadding=\"0\" cellspacing=\"0\"><tr><td width=\"250\" valign=\"top\"> \n" +
             "               <table cellpadding=\"0\" cellspacing=\"0\" class=\"es-left\" align=\"left\" style=\"mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:left;\"> \n" +
             "                 <tr style=\"border-collapse:collapse;\"> \n" +
             "                  <td width=\"250\" class=\"es-m-p20b\" align=\"left\" style=\"padding:0;Margin:0;\"> \n" +
@@ -282,7 +282,7 @@ public class TemplateUtils {
             "                   </table></td> \n" +
             "                 </tr> \n" +
             "               </table> \n" +
-            "               <!--[if mso]></td><td width=\"20\"></td><td width=\"250\" valign=\"top\"><![endif]--> \n" +
+            "               <!--[if mso]></td><td width=\"20\"></td><td width=\"250\" valign=\"top\"> \n" +
             "               <table cellpadding=\"0\" cellspacing=\"0\" class=\"es-right\" align=\"right\" style=\"mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:right;\"> \n" +
             "                 <tr style=\"border-collapse:collapse;\"> \n" +
             "                  <td width=\"250\" align=\"left\" style=\"padding:0;Margin:0;\"> \n" +
@@ -293,7 +293,7 @@ public class TemplateUtils {
             "                   </table></td> \n" +
             "                 </tr> \n" +
             "               </table> \n" +
-            "               <!--[if mso]></td></tr></table><![endif]--></td> \n" +
+            "               <!--[if mso]></td></tr></table></td> \n" +
             "             </tr> \n" +
             "           </table></td> \n" +
             "         </tr> \n" +
@@ -315,17 +315,17 @@ public class TemplateUtils {
             "         text-decoration: none;\n" +
             "         }\n" +
             "      </style>\n" +
-            "      <![endif]-->\n" +
+            "      \n" +
             "      <!--[if gte mso 9]>\n" +
             "      <style>sup {\n" +
             "         font-size: 100% !important;\n" +
             "         }\n" +
             "      </style>\n" +
-            "      <![endif]-->\n" +
+            "      \n" +
             "      <!--[if !mso]><!-- -->\n" +
             "      <link href=\"https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i\" rel=\"stylesheet\">\n" +
             "      <link href=\"https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i\" rel=\"stylesheet\">\n" +
-            "      <!--<![endif]-->\n" +
+            "      <!--\n" +
             "      <style type=\"text/css\">\n" +
             "         @media only screen and (max-width: 600px) {\n" +
             "         p, ul li, ol li, a {\n" +
@@ -503,7 +503,7 @@ public class TemplateUtils {
             "         <v:background xmlns:v=\"urn:schemas-microsoft-com:vml\" fill=\"t\">\n" +
             "            <v:fill type=\"tile\" color=\"#f1f1f1\"></v:fill>\n" +
             "         </v:background>\n" +
-            "         <![endif]-->\n" +
+            "         \n" +
             "         <table class=\"es-wrapper\" width=\"100%\" cellspacing=\"0\" cellpadding=\"0\"\n" +
             "            style=\"mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;padding:0;Margin:0;width:100%;height:100%;background-repeat:repeat;background-position:center top;\">\n" +
             "         <tr style=\"border-collapse:collapse;\">\n" +
