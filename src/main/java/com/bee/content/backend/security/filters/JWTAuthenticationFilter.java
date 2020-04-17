ff318f67b407d6a856048dc34e0b24047514d00b
Patrizio Pezzilli <patriziopezzilli@gmail.com>
2020-04-17 22:46:18 +0200

    merchant management on security

--- src/main/java/com/bee/content/backend/security/filters/JWTAuthenticationFilter.java
+++ src/main/java/com/bee/content/backend/security/filters/JWTAuthenticationFilter.java
@@ -1,25 +1,11 @@
 package com.bee.content.backend.security.filters;
 
-import static com.bee.content.backend.constants.SecurityConstants.HEADER_STRING;
-import static com.bee.content.backend.constants.SecurityConstants.SECRET;
-import static com.bee.content.backend.constants.SecurityConstants.TOKEN_PREFIX;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
-
-import com.bee.content.backend.service.UserService;
 import com.bee.content.backend.dto.ApplicationUser;
+import com.bee.content.backend.service.UserService;
 import com.bee.content.backend.utils.ThreadState;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,33 +18,39 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import static com.bee.content.backend.constants.SecurityConstants.*;
+
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-	
-	Logger logger = LoggerFactory.getLogger(JWTAuthenticationFilter.class);
-	
-	@Autowired
+
+    Logger logger = LoggerFactory.getLogger(JWTAuthenticationFilter.class);
+
+    @Autowired
     UserService userService;
-	
+
     private AuthenticationManager authenticationManager;
-    
+
     public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
-       
+
     }
-    
+
     @Override
     public Authentication attemptAuthentication(HttpServletRequest req,
                                                 HttpServletResponse res) throws AuthenticationException {
         try {
-        	
-			InputStream ist = req.getInputStream();
-			res.setHeader("Access-Control-Allow-Headers", "Authorization");
-			res.setHeader("Access-Control-Expose-Headers", "Authorization");
-			logger.debug("Attempting for Authentication......");
+
+            InputStream ist = req.getInputStream();
+            res.setHeader("Access-Control-Allow-Headers", "Authorization");
+            res.setHeader("Access-Control-Expose-Headers", "Authorization");
+            logger.debug("Attempting for Authentication......");
 
             ApplicationUser creds = new ObjectMapper()
                     .readValue(ist, ApplicationUser.class);
@@ -73,24 +65,25 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             throw new RuntimeException(e);
         }
     }
+
     @Override
     protected void successfulAuthentication(HttpServletRequest req,
                                             HttpServletResponse res,
                                             FilterChain chain,
                                             Authentication auth) throws IOException, ServletException {
-    	
-    	logger.debug("Successfully authenticated....");
-    	List<String> roles = new ArrayList<String>();
-    	
-    	for (GrantedAuthority authority : auth.getAuthorities() ) {
-    		roles.add(authority.getAuthority());
-    	}
-    	
-    	ApplicationUser loggedInUser = new ApplicationUser();
-    	String userName = ((User) auth.getPrincipal()).getUsername();
-    	loggedInUser.setEmail(userName);
-    	loggedInUser.setRoles(roles);
-    	logger.debug("Creating jwt token for the user");
+
+        logger.debug("Successfully authenticated....");
+        List<String> roles = new ArrayList<String>();
+
+        for (GrantedAuthority authority : auth.getAuthorities()) {
+            roles.add(authority.getAuthority());
+        }
+
+        ApplicationUser loggedInUser = new ApplicationUser();
+        String userName = ((User) auth.getPrincipal()).getUsername();
+        loggedInUser.setEmail(userName);
+        loggedInUser.setRoles(roles);
+        System.out.println("Merchant " + ThreadState.INSTANCE.getMerchant());
         String token = Jwts.builder()
                 .signWith(SignatureAlgorithm.HS512, SECRET)
                 .setExpiration(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000))
@@ -101,7 +94,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .claim("merchant", ThreadState.INSTANCE.getMerchant())
                 .claim("roles", roles)
                 .compact();
-        
+
         logger.debug("Appending token in the response.");
         res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
 
@@ -112,17 +105,17 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         JSONObject jsonObject = new JSONObject(jsonMap);
         res.getWriter().write(jsonObject.toString());
     }
-    
+
     @Override
     public void setPasswordParameter(String passwordParameter) {
-    	super.setPasswordParameter("password");
+        super.setPasswordParameter("password");
     }
-    
-    
+
+
     @Override
     public void setUsernameParameter(String usernameParameter) {
-    	super.setUsernameParameter("email");
+        super.setUsernameParameter("email");
     }
-    
-    
+
+
 }
